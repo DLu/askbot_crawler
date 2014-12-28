061a91cdacfd774a530c7362ef538c39c9ab8d80
David Lu <davidvlu@gmail.com>
2014-12-27 23:06:53 -0500

    Multiple data files

--- question_database.py
+++ question_database.py
@@ -1,29 +1,44 @@
 from question import Question
 from crawler import *
 import yaml
+import collections
+import os
 
 DATA_FOLDER = 'data/'
 
 class QuestionDatabase:
     def __init__(self):
         self.data = {}
-        for qid, data in yaml.load(open(DATA_FOLDER + 'questions.yaml')).iteritems():
-            q = Question(data)
-            self.data[qid] = q
+        for fn in os.listdir(DATA_FOLDER):
+            for qid, data in yaml.load(open(DATA_FOLDER + fn)).iteritems():
+                q = Question(data)
+                self.data[qid] = q
     
     def close(self):
-        data = {}
+        data = collections.defaultdict(dict)
         for qid, q in self.data.iteritems():
-            data[qid] = q.data()
-        yaml.dump(data, open(DATA_FOLDER + 'questions.yaml', 'w'))
+            bucket = qid - (qid % 1000)
+            data[bucket][qid] = q.data()
+        for bucket in sorted(data):
+            fn = 'questions%07d.yaml'%bucket
+            print 'Writing',fn
+            yaml.dump(data[bucket], open(DATA_FOLDER + fn, 'w'))
         
     def add_questions(self, qs):
         for q in qs:
             self.data[ q.id ] = q
             
+    def update(self):
+        n = len(self.data)
+        pn = n/50+1
+        print "Load page %d"%pn
+        self.add_questions( load_question_page(page=pn, sort='activity-asc') )
+            
         
         
 if __name__=='__main__':
     db = QuestionDatabase()
-    db.add_questions( load_question_page() )
+    for x in range(10):
+        db.update()
+        print len(db.data)
     db.close()
