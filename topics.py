2eec7d8cd054b08a74b15d0d21f7e56fbf8586f6
David V. Lu!! <davidvlu@gmail.com>
2015-01-16 18:29:30 -0600

    Change page name

--- topics.py
+++ topics.py
@@ -28,7 +28,7 @@ def generate_topic_page(db, topic, questions):
         f.write( s2 )
 
 
-def generate_topics_page(db, fn='website/table.html'):
+def generate_topics_page(db, fn='website/topics.html'):
     data = []
     #print '<tr><th>Tag<th># answers<th># answered<th># questions'
     m = db.get_topic_map()
